1b49fb5558bee8697d00295d9401f63588842972
ELTheo77 <teodor.leahu@saguna.ro>
2022-07-26 12:04:27 +0300

    Day2_Task1

---
+++ CodeSinaia-2022.src-1/Python/Labs/MadLibs/text_editor.py
@@ -0,0 +1,31 @@
+with open('simple.txt') as f:
+    lines = f.readlines()
+text=""
+count = 0
+for line in lines:
+    count += 1
+    res=line.split()
+    rezultat=""
+    for word in res:
+        if word[0]=='<' or (word[0]=='"' and word[1]=='<'):
+            print("\nSelect a/an ", end="")
+            i=1
+            while word[i]!='>':
+                print(word[i], end="")
+                i+=1
+            print("\n")
+            new_word=input()
+            print("\n")
+            rezultat+=new_word
+            i+=1
+            while i<len(word):
+                rezultat+=word[i]
+                i+=1
+            rezultat+=" "
+        else:
+            rezultat+=word+" "
+    text+=rezultat+"\n"
+    print(f'{rezultat}')
+print("\n")
+print(text)
+f.close()
