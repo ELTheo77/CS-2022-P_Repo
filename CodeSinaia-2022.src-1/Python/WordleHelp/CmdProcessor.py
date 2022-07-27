ecaa76544c9c0f1b81ed56943d1378fc70855d39
ELTheo77 <teodor.leahu@saguna.ro>
2022-07-27 15:10:45 +0300

    Day3_Task1

--- CodeSinaia-2022.src-1/Python/WordleHelp/CmdProcessor.py
+++ CodeSinaia-2022.src-1/Python/WordleHelp/CmdProcessor.py
@@ -1,10 +1,34 @@
+###
+# Class CmdProcessor is in charge of implementing all commands supported by WordleHelp
+from WordChecker import WordChecker
+#from Scraper import Scraper
+from Scraper import Scraper
+
 class CmdProcessor:
+    ###
+    # Class fields
+    # _wordChecker: the "engine" in charge of storing and using all hints
+    #               used for verifying words from the database
+
+    ###
+    # Class constructor
+    def __init__(self):
+        self._wordChecker = WordChecker()
 
     def processHelp(self):
         print("process command 'help'")
 
-    def processAdd(self, args = None):
-        print("Process command 'add'")
+    def processAdd(self, args):
+        print("Processing command 'add'")
+        Dict = {}
+        line = Scraper(args)
+        #print(line._text)
+        res=line._text.split()
+        i=0
+        for cuv in res:
+            Dict[i]=cuv
+            i+=1
+        print(Dict)
 
     def processMatch(self, args = None):
         print("Process command 'match'")
@@ -18,11 +42,8 @@ class CmdProcessor:
     def processConfig(self, args = None):
         print("Process command 'config'")
 
+###
+# CmdProcessor test code
 if __name__ == "__main__":
     cmdP = CmdProcessor()
     cmdP.processHelp()
-    print(cmdP.semnatura)
-    cmdP2 = CmdProcessor()
-    cmdP2.processAdd()
-    print(cmdP2.semnatura)
-    print(cmdP.semnatura)
