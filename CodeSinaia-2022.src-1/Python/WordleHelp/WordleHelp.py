5b71c401112c1fa08cb0a93a451cdab92ed74b48
ELTheo77 <teodor.leahu@saguna.ro>
2022-07-27 15:10:17 +0300

    Day3_Task1

--- CodeSinaia-2022.src-1/Python/WordleHelp/WordleHelp.py
+++ CodeSinaia-2022.src-1/Python/WordleHelp/WordleHelp.py
@@ -2,28 +2,18 @@
 # Main code for WordleHelper program
 # Implements the command loop and uses a CmdProcessor
 # object to process each of the supported commands
-
-Dict = {}
-with open('words.txt') as f:
-    lines = f.readlines()
-for line in lines:
-    res=line.split()
-    Dict[res[0]]=int(res[1])
-f.close()
-print(Dict)
-
 import CmdProcessor as cp
 
 if __name__ == "__main__":
     print("This is Wordle helper!")
-    #cmdProcessor = cp.CmdProcessor()
-    cmdProcessor = None
+    cmdProcessor = cp.CmdProcessor()
     line = input("Command?> ").lower()
     while(line != "quit" and line != "exit"):
         if line == "?" or line == "help":
             cmdProcessor.processHelp()
         elif line.startswith("add"):
-            cmdProcessor.processAdd(line)
+            res=line.split()
+            cmdProcessor.processAdd(res[1])
         elif line.startswith("match"):
             cmdProcessor.processMatch(line)
         elif line.startswith("reset"):
